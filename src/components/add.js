659d50f5e9c2cac91fa7fdc3d7894279ce4f68d8
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-16 12:56:33 +0200

    added br line between input elements

--- src/components/add.js
+++ src/components/add.js
@@ -6,10 +6,15 @@ function User(){
             </div>
             <div class="box2">
             <input type="text" placeholder="Enter name"/>
+            <br/>
             <input type="text" placeholder="Enter surname"/>
+            <br/>
             <input type="text" placeholder="Enter email address"/>
+            <br/>
             <input type="text" placeholder="Cell number"/>
+            <br/>
             <input type="text" placeholder="Position"/>
+            <br/>
             <input type="text" placeholder="Enter department"/>
             <button>Submit</button>
             </div>
