93123f2a72b1ff319c21353d39347b1bb760e51d
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-18 11:15:09 +0200

    Added image input

--- src/pages/user.js
+++ src/pages/user.js
@@ -26,7 +26,8 @@ function User(props){
 
     return(
         <div class="wrapper"> 
-            <button onClick={handleSubmit}>Switch to Admin</button>
+            <button onClick={handleSubmit} class="switchBtn">Switch to Admin</button>
+            <div class="infoBox">
             <div class="box1">
             <h1>User Page</h1>
             <input type="text" placeholder="Enter name" onChange={(event)=> setName(event.target.value)} />
@@ -35,17 +36,26 @@ function User(props){
             <br/>
             <input type="text" placeholder="Enter email address" onChange={(event)=> setEmail(event.target.value)}/>
             <br/>
-            <input type="text" placeholder="Cell number" onChange={(event)=> setCell(event.target.value)}/>
+            <input type="number" placeholder="Cell number" onChange={(event)=> setCell(event.target.value)}/>
             <br/>
             <input type="text" placeholder="Position" onChange={(event)=> setPosition(event.target.value)}/>
             <br/>
             <input type="text" placeholder="Enter department" onChange={(event)=> setDepartment(event.target.value)}/>
             <br/>
-            <button onClick={add}>Submit</button>
+            <div class="hero">
+                <div class="card">
+                    <img src="https://i0.wp.com/digitalhealthskills.com/wp-content/uploads/2022/11/3da39-no-user-image-icon-27.png?fit=500%2C500&ssl=1"/>
+                    <input type="file" accept="image/JPEG, image/png, image/jpg" id="input-file"/>
+                    <label for="input-file" id="update-img">Update</label>
+                </div>
+            </div>
+            <button onClick={add} id="submit-btn">Submit</button>
             </div>
             <div class="box2">
             <img src="/media/4115334.jpg" alt="illustration of an employee portal"/>
             </div>
+            </div>
+            
             </div>
     );
 }
