738ea578cb7ed216b22265c30c21549959821f90
Reabetswe <140710640+ReaDaCoder@users.noreply.github.com>
2024-07-18 12:06:43 +0200

    added switch button in admin file

--- src/pages/admin.js
+++ src/pages/admin.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import Heading from '../components/heading';
+import { useNavigate } from 'react-router-dom'
 
 
-export default function Portal (){
+export default function Portal (props){
+        const navigate = useNavigate();
+    
+        const handleSubmit = () => {
+            // Navigate to the admin page
+            navigate('/user');
+        }
+
+
     return(
         <div className="container">
             <Heading/>
+            <button onClick={handleSubmit} class="switchBtn">Switch to User</button>
             <h1>User Management</h1>
-            <input type="text" placeholder="Search User"/>
+            <input type="text" placeholder="Search User" class="searchUserInput"/>
             <button class="searchUser">Search User</button>
           <div className="grid">
             <div class="box3">
@@ -23,4 +33,5 @@ export default function Portal (){
 
         </div>
     );
+
 }
